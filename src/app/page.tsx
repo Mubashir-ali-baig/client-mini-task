96aa0046d033d2e43d1baf9135b3401d30c70360
Mubashir Ali Baig <mubashir@Mubashirs-MacBook-Air.local>
2025-05-08 17:49:50 +0500

    button text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center gap-6">
         <h2 className="font-bold text-xl">Button Component</h2>
         <div className="flex flex-wrap gap-4">
-          <Button variant="primary">Primary Button</Button>
-          <Button variant="secondary">Secondary Button</Button>
+          <Button variant="primary">CREATE NEW COIN</Button>
+          <Button variant="secondary">EDIT PROFILE</Button>
         </div>
       </div>
     </div>
